28c6d72ce71bd2a5b9b50c2c7956a0a15415760f
AJ Kumar <ajanthan29894@gmail.com>
2019-06-21 15:33:21 +0100

    added foundation for graphqlinto node app

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const app = express();
 
@@ -8,5 +10,36 @@ app.use(bodyParser.json());
 app.get('/', (req, res, next) => {
   res.send('Hello World!');
 });
+
+app.use(
+  '/graphql',
+  graphqlHttp({
+    //! means its not nullable - must return something even if empty.
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+
+        type RootMutation {
+            createEvent(name: String): String
+        }
+
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+      events: () => {
+        return ['Birthday', 'Cooking', 'Sailing'];
+      },
+      createEvent: args => {
+        const eventName = args.name;
+        return eventName;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(3000);
