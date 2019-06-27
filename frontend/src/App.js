e85b296e22e0c309601be73ebfa964467d217bd8
AJ Kumar <ajanthan29894@gmail.com>
2019-06-27 12:45:57 +0100

    added CSS and navbar to app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter, Route, Redirect, Link, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/navigation/MainNavigation';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Redirect from='/' to='/auth' exact />
-        <Route path='/auth' component={AuthPage} />
-        <Route path='/events' component={EventsPage} />
-        <Route path='/bookings' component={BookingsPage} />
-      </Switch>
+      <MainNavigation />
+      <main className='main-content'>
+        <Switch>
+          <Redirect from='/' to='/auth' exact />
+          <Route path='/auth' component={AuthPage} />
+          <Route path='/events' component={EventsPage} />
+          <Route path='/bookings' component={BookingsPage} />
+        </Switch>
+      </main>
     </BrowserRouter>
   );
 }
