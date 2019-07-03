902e1af10b40c51f4b773376953cfbee836e6df5
AJ Kumar <ajanthan29894@gmail.com>
2019-07-03 16:54:31 +0100

    Used dataloader to improve performance. It prevents duplicate requests being performed and thus batches requests made to the database and makes unique requests. It then sends the responses to whichever function made the original calls. check comments in merge.js

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,6 +1,20 @@
 const Event = require('../../models/event');
 const User = require('../../models/user');
 const { dateToString } = require('../../helpers/date');
+const DataLoader = require('dataloader');
+
+// Dataloader is a batching mechanisms. Multiple requests (database requests) are batched together
+// If 4 requests with the same ID is requested to dataloader only one request is sent and the response is sent
+// to each of the 4 functions that originally made the request
+// Only unique requests are made as a result, improving performance
+
+const eventLoader = new DataLoader(eventIds => {
+  return events(eventIds);
+});
+
+const userLoader = new DataLoader(userIds => {
+  return User.find({ _id: { $in: userIds } });
+});
 
 // async await is another syntax for promises. can be used instead of .then
 const events = async eventIds => {
@@ -16,11 +30,12 @@ const events = async eventIds => {
 
 const user = async userId => {
   try {
-    const user = await User.findById(userId);
+    const user = await userLoader.load(userId.toString());
 
     return {
       ...user._doc,
-      createdEvents: events.bind(this, user._doc.createdEvents),
+      _id: user.id,
+      createdEvents: eventLoader.load.bind(this, user._doc.createdEvents),
       password: null
     };
   } catch (err) {
@@ -30,8 +45,10 @@ const user = async userId => {
 
 const singleEvent = async eventId => {
   try {
-    const event = await Event.findById(eventId);
-    return transformEvent(event);
+    //instead of making a call to mongodb again, you can use dataloader to see if the data has already been requested before - reduces number of calls
+    // increased performance as a result
+    const event = await eventLoader.load(eventId.toString());
+    return event;
   } catch (err) {
     throw err;
   }
